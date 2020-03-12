9ea6554cbbedab84249e6d7ad8f5ca2aa928f3f2
Yutao Gu <copperion@outlook.com>
2020-03-12 16:58:01 +0800

    try to add clash package

--- diy.sh
+++ diy.sh
@@ -6,4 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
+# Add Luci-App-Clash Package
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+./scripts/feeds update -a
+./scripts/feeds install -a
+
