2d1d10b5ade1a6eac59e3c9afcac26f36b7e4bb6
Yutao Gu <copperion@outlook.com>
2020-03-12 17:06:41 +0800

    add rosy theme

--- diy.sh
+++ diy.sh
@@ -2,13 +2,11 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
 # Add Luci-App-Clash Package
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/feeds/luci/luci-theme-rosy
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
